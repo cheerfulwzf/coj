a8e48505499627e9914816268734e053046b95d0
wangzhifu_i <wangzhifu_i@didiglobal.com>
2022-08-18 20:40:46 +0800

    optimize imports

---
+++ oj-judge/src/main/java/com/cheerful/oj/judge/factory/impl/OtherJudgeHandler.java
@@ -0,0 +1,7 @@
+package com.cheerful.oj.judge.factory.impl;/**
+ * @Copyright: www.xiaojukeji.com Inc. All rights reserved.
+ * @Description:
+ * @Date: 8/18/22 7:19 PM
+ * @Author: wangzhifu
+ */public class OtherJudgeHandler {
+}
