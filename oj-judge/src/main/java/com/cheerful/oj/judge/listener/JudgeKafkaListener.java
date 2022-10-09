ef4c8f268d6ffcf3d61e4c309bb046e720ef1a77
wangzhifu_i <wangzhifu_i@didiglobal.com>
2022-10-09 11:04:12 +0800

    perf:kafka修改ack

--- oj-judge/src/main/java/com/cheerful/oj/judge/listener/JudgeKafkaListener.java
+++ oj-judge/src/main/java/com/cheerful/oj/judge/listener/JudgeKafkaListener.java
@@ -3,8 +3,11 @@ package com.cheerful.oj.judge.listener;
 import com.cheerful.oj.common.dto.JudgeTaskDTO;
 import com.cheerful.oj.common.util.JsonUtil;
 import com.cheerful.oj.judge.service.JudgeService;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 /**
@@ -24,8 +27,20 @@ public class JudgeKafkaListener {
 	}
 
 	@KafkaListener
-	public void judgeTaskHandler(JudgeTaskDTO taskDTO) {
-		log.info("接到判题任务:{}", JsonUtil.toJsonString(taskDTO));
-		judgeService.judge(taskDTO);
+	public void judgeTaskHandler(ConsumerRecord<String, String> msg, Acknowledgment ack) {
+		Optional<String> s = Optional.ofNullable(msg.value());
+		if (s.isPresent()) {
+			try {
+				String str = s.get();
+				log.info("接到判题任务:{}", str);
+				JudgeTaskDTO task = JsonUtil.fromJson(str, JudgeTaskDTO.class);
+				judgeService.judge(task);
+			} catch (Exception e) {
+				// TODO: 10/9/22 失败的消息可以存在一个地方，这里先仅放一个日志
+				log.error("task:{} error, \nerror details:{}", msg.value(), e.getCause());
+			} finally {
+				ack.acknowledge();
+			}
+		}
 	}
 }
